f9fed1d26d9eb132819a3179dd98b7ee981583b7
José Linhares <jlinhares@sapo.pt>
2016-07-05 20:29:08 +0000

    Read me about video-background

--- video-background/videobackground.component.ts
+++ video-background/videobackground.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit, Input} from '@angular/core'
 
 @Component({
     selector:'video-background',    
-    template:'<div (window:resize)="onResize()" style="block;width:100vw; height:100vh;z-index:-1;position: absolute;overflow: hidden;">'+
+    template:'<div (window:resize)="onResize()" style="width:100vw; height:100vh;display:block;z-index:-1;position: absolute;overflow: hidden;">'+
              '<video [muted]="!sound" [style.top]="vtop" [style.left]="vleft" [style.width]="vwidth" [style.height]="vheight" style="position:relative;" autoplay loop preload ><ng-content></ng-content></video>'+
              '</div>'
 })
@@ -18,31 +18,20 @@ export class VideoBackgroundComponent implements OnInit {
 
     //Implements
     ngOnInit(){
-        var whpx=window.innerHeight;
-        var wwpx=window.innerWidth;        
-        if(wwpx/whpx>1280/720){
-            this.vheight=null;
-            this.vwidth='100vw';
-            this.vleft=0;
-            var vh= wwpx*720/1280;
-            this.vtop=Math.round((whpx-vh)/2)+'px';
-        }
-        else{
-            this.vheight='100vh';
-            this.vwidth=null;
-            this.vtop=0;
-            var vw=whpx*1200/720;
-            this.vleft=Math.round((vw-wwpx)/2)+'px';
-        }        
+        this.resize();        
     }
 
     //DOM events
     onResize(){       
-        var wh=window.innerHeight;
+        this.resize();
+    }
+
+    private resize() {
+        var wh =window.innerHeight;
         var ww=window.innerWidth;
         if(ww/wh > 1280/720){
             this.vheight=null;
-            this.vwidth='100vw';
+            this.vwidth= '100vw';                        
             this.vleft=0;
             this.vtop=Math.round((wh-ww*720/1280)/2)+'px';
         }
@@ -50,7 +39,9 @@ export class VideoBackgroundComponent implements OnInit {
             this.vheight='100vh';
             this.vwidth=null;
             this.vtop=0;
-            this.vleft=Math.round((ww-wh*1280/720)/2)+'px';              
+            this.vheight= '100vh';
+            var vw=wh*1200/720;
+            this.vleft=Math.round((ww-wh*1280/720)/2)+'px';  
         }
     }
 }
