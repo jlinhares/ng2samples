6beeb7540ad52fffc9f966eb9289396570c7a951
José Linhares <jlinhares@sapo.pt>
2016-07-08 23:35:10 +0000

    Video background component code

--- video-background/videobackground.component.ts
+++ video-background/videobackground.component.ts
@@ -1,47 +1,90 @@
 import {Component, OnInit, Input} from '@angular/core'
 
+class VBGBox{
+    width: number;
+    height: number;
+}
+
+class VBGZone extends VBGBox{
+    top: number;
+    left: number;
+}
+
+class VBGVideo extends VBGZone{
+    sound:boolean;
+}
+
+class VBGInner extends VBGBox{
+    constructor(){
+        super();
+        this.width= window.innerWidth;
+        this.height= window.innerHeight;
+    }
+    ratio(){
+       return this.width/this.height;
+    }
+}
+
 @Component({
-    selector:'video-background',    
-    template:'<div (window:resize)="onResize()" style="width:100vw; height:100vh;display:block;z-index:-1;position: absolute;overflow: hidden;">'+
-             '<video [muted]="!sound" [style.top]="vtop" [style.left]="vleft" [style.width]="vwidth" [style.height]="vheight" style="position:relative;" autoplay loop preload ><ng-content></ng-content></video>'+
+    selector:'video-background',
+    template:'<div (window:resize)="onResize()" [style.min-width]="min.width+\'px\'" [style.min-height]="min.height+\'px\'" style="width:100vw; height:100vh;display:block;z-index:-1;position: absolute;overflow: hidden;">'+
+             '<video [muted]="!video.sound" [style.top]="video.top" [style.left]="video.left" [style.width]="video.width" [style.height]="video.height" style="position:relative;" autoplay loop preload ><ng-content></ng-content></video>'+
              '</div>'
 })
 export class VideoBackgroundComponent implements OnInit {
     @Input () sound: boolean;
-    vheight='';
-    vwidth='';
-    vtop=null;
-    vleft=null;    
+    main: VBGZone;
+    video: VBGVideo;
+    min: VBGBox;    
+    @Input('min-height') minHeight: number;
+    @Input('min-width') minWidth: number;    
+    
 
-    constructor (){    
+    constructor (){         
+        this.video= new VBGVideo;
+        this.main= new VBGZone;
+        this.min= new VBGBox;
     }
 
     //Implements
-    ngOnInit(){
-        this.resize();        
+    ngOnInit(){                
+        this.min.width=this.minWidth;
+        this.min.height=this.minHeight;
+        this.calc_resize();
     }
 
     //DOM events
-    onResize(){       
-        this.resize();
+    onResize(){
+        this.calc_resize();
     }
 
-    private resize() {
-        var wh =window.innerHeight;
-        var ww=window.innerWidth;
-        if(ww/wh > 1280/720){
-            this.vheight=null;
-            this.vwidth= '100vw';                        
-            this.vleft=0;
-            this.vtop=Math.round((wh-ww*720/1280)/2)+'px';
+    private calc_resize() {   
+        var inner= new VBGInner;        
+        if( inner.ratio() > 1280/720){
+            if( inner.width < this.min.width ){
+                this.video.width=this.min.width;
+                this.video.top=Math.round((inner.height - this.min.width*720/1280)/2);
+            }
+            else{
+                this.video.width= inner.width;
+                this.video.top=Math.round((inner.height- inner.width*720/1280)/2); 
+            }
+            this.video.height=null;                                  
+            this.video.left=0;
         }
         else{
-            this.vheight='100vh';
-            this.vwidth=null;
-            this.vtop=0;
-            this.vheight= '100vh';
-            var vw=wh*1200/720;
-            this.vleft=Math.round((ww-wh*1280/720)/2)+'px';  
+            if(inner.height<this.min.height){
+                this.video.height=this.minHeight;
+                this.video.left=Math.round((inner.width-this.min.height*1280/720)/2);
+            }
+            else{
+                this.video.height=inner.height;
+                this.video.left=Math.round((inner.width-inner.height*1280/720)/2);
+            }
+            this.video.width=null;
+            this.video.top=0;   
         }
     }
 }
+
+
