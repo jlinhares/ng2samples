06571c2ae399dbb96d7f5017b42067c4e374c341
José Linhares <jlinhares@sapo.pt>
2016-09-22 12:00:06 +0000

    Video background module

---
+++ video-background/videobackground.module.ts
@@ -0,0 +1,10 @@
+import { NgModule }      from '@angular/core';
+import {VideoBackgroundComponent} from './videobackground.component';
+
+@NgModule({
+  exports:[VideoBackgroundComponent],
+  declarations: [ VideoBackgroundComponent ]
+})
+export class VideoBackgroundModule { 
+
+}
